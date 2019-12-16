46172cd33457ec5f8b80a504bc1379109752a14a
neelu0 <neelu0@protonmail.com>
2019-12-16 15:19:29 +0500

    revamped blog to anonymous board with bare minimum stuff

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,12 +2,9 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
-from flaskblog import app, db, bcrypt, mail
-from flaskblog.forms import (RegistrationForm, LoginForm, UpdateAccountForm,
-                             PostForm, RequestResetForm, ResetPasswordForm, SubPostForm)
-from flaskblog.models import User, Post, SubPost
-from flask_login import login_user, current_user, logout_user, login_required
-from flask_mail import Message
+from flaskblog import app, db
+from flaskblog.forms import (PostForm, SubPostForm)
+from flaskblog.models import Post, SubPost
 
 
 @app.route("/")
@@ -21,43 +18,6 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
-
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. Please check email and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('home'))
 
 
 def save_picture(form_picture):
@@ -74,33 +34,12 @@ def save_picture(form_picture):
     return picture_fn
 
 
-@app.route("/account", methods=['GET', 'POST'])
-@login_required
-def account():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('account'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account',
-                           image_file=image_file, form=form)
-
 
 @app.route("/post/new", methods=['GET', 'POST'])
-@login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data, content=form.content.data)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
@@ -117,45 +56,42 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post, subposts=subposts)
 
 
-@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
-@login_required
-def update_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        abort(403)
-    form = PostForm()
-    if form.validate_on_submit():
-        post.title = form.title.data
-        post.content = form.content.data
-        db.session.commit()
-        flash('Your post has been updated!', 'success')
-        return redirect(url_for('post', post_id=post.id))
-    elif request.method == 'GET':
-        form.title.data = post.title
-        form.content.data = post.content
-    return render_template('create_post.html', title='Update Post',
-                           form=form, legend='Update Post')
+# @app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+# def update_post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     if post.author != current_user:
+#         abort(403)
+#     form = PostForm()
+#     if form.validate_on_submit():
+#         post.title = form.title.data
+#         post.content = form.content.data
+#         db.session.commit()
+#         flash('Your post has been updated!', 'success')
+#         return redirect(url_for('post', post_id=post.id))
+#     elif request.method == 'GET':
+#         form.title.data = post.title
+#         form.content.data = post.content
+#     return render_template('create_post.html', title='Update Post',
+#                            form=form, legend='Update Post')
 
 
-@app.route("/post/<int:post_id>/delete", methods=['POST'])
-@login_required
-def delete_post(post_id):
-    post = Post.query.get_or_404(post_id)
-    if post.author != current_user:
-        abort(403)
-    SubPost.query.filter(SubPost.post_id == post_id).delete(synchronize_session=False)
-    db.session.delete(post)
-    db.session.commit()
-    flash('Your post has been deleted!', 'success')
-    return redirect(url_for('home'))
+# @app.route("/post/<int:post_id>/delete", methods=['POST'])
+# def delete_post(post_id):
+#     post = Post.query.get_or_404(post_id)
+#     if post.author != current_user:
+#         abort(403)
+#     SubPost.query.filter(SubPost.post_id == post_id).delete(synchronize_session=False)
+#     db.session.delete(post)
+#     db.session.commit()
+#     flash('Your post has been deleted!', 'success')
+#     return redirect(url_for('home'))
 
 @app.route("/post/<int:post_id>/subpost/new", methods=['POST','GET'])
-@login_required
 def new_subpost(post_id):
     redirectpostid=post_id
     form = SubPostForm()
     if form.validate_on_submit():
-        subpost = SubPost(content=form.content.data, author=current_user, post_id=post_id)
+        subpost = SubPost(content=form.content.data, post_id=post_id)
         db.session.add(subpost)
         db.session.commit()
         flash('Your subpost has been created!', 'success')
@@ -163,87 +99,31 @@ def new_subpost(post_id):
     return render_template('create_subpost.html', form=form)
 
 
-@app.route("/post/<int:post_id>/subpost/<int:subpost_id>/update", methods=['GET', 'POST'])
-@login_required
-def update_subpost(post_id,subpost_id):
-    post = Post.query.get_or_404(post_id)
-    subpost = SubPost.query.get_or_404(subpost_id)
-    if subpost.author != current_user:
-        abort(403)
-    form = SubPostForm()
-    if form.validate_on_submit():
-        subpost.content = form.content.data
-        db.session.commit()
-        flash('Your post has been updated!', 'success')
-        return redirect(url_for('post', post_id=post.id))
-    elif request.method == 'GET':
-        form.content.data = subpost.content
-    return render_template('create_subpost.html', title='Update SubPost',
-                           form=form, legend='Update SubPost')
+# @app.route("/post/<int:post_id>/subpost/<int:subpost_id>/update", methods=['GET', 'POST'])
+# def update_subpost(post_id,subpost_id):
+#     post = Post.query.get_or_404(post_id)
+#     subpost = SubPost.query.get_or_404(subpost_id)
+#     if subpost.author != current_user:
+#         abort(403)
+#     form = SubPostForm()
+#     if form.validate_on_submit():
+#         subpost.content = form.content.data
+#         db.session.commit()
+#         flash('Your post has been updated!', 'success')
+#         return redirect(url_for('post', post_id=post.id))
+#     elif request.method == 'GET':
+#         form.content.data = subpost.content
+#     return render_template('create_subpost.html', title='Update SubPost',
+#                            form=form, legend='Update SubPost')
 
 
-@app.route("/post/<int:post_id>/subpost/<int:subpost_id>/delete", methods=['POST','GET'])
-@login_required
-def delete_subpost(post_id, subpost_id):
-    post = Post.query.get_or_404(post_id)
-    subpost = SubPost.query.get_or_404(subpost_id)
-    if subpost.author != current_user:
-        abort(403)
-    db.session.delete(subpost)
-    db.session.commit()
-    flash('Your post has been deleted!', 'success')
-    return redirect(url_for('post',post_id=post.id))
-
-
-@app.route("/user/<string:username>")
-def user_posts(username):
-    page = request.args.get('page', 1, type=int)
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(author=user)\
-        .order_by(Post.date_posted.desc())\
-        .paginate(page=page, per_page=5)
-    return render_template('user_posts.html', posts=posts, user=user)
-
-
-def send_reset_email(user):
-    token = user.get_reset_token()
-    msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
-                  recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
-{url_for('reset_token', token=token, _external=True)}
-
-If you did not make this request then simply ignore this email and no changes will be made.
-'''
-    mail.send(msg)
-
-
-@app.route("/reset_password", methods=['GET', 'POST'])
-def reset_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RequestResetForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        send_reset_email(user)
-        flash('An email has been sent with instructions to reset your password.', 'info')
-        return redirect(url_for('login'))
-    return render_template('reset_request.html', title='Reset Password', form=form)
-
-
-@app.route("/reset_password/<token>", methods=['GET', 'POST'])
-def reset_token(token):
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    user = User.verify_reset_token(token)
-    if user is None:
-        flash('That is an invalid or expired token', 'warning')
-        return redirect(url_for('reset_request'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user.password = hashed_password
-        db.session.commit()
-        flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('reset_token.html', title='Reset Password', form=form)
+# @app.route("/post/<int:post_id>/subpost/<int:subpost_id>/delete", methods=['POST','GET'])
+# def delete_subpost(post_id, subpost_id):
+#     post = Post.query.get_or_404(post_id)
+#     subpost = SubPost.query.get_or_404(subpost_id)
+#     if subpost.author != current_user:
+#         abort(403)
+#     db.session.delete(subpost)
+#     db.session.commit()
+#     flash('Your post has been deleted!', 'success')
+#     return redirect(url_for('post',post_id=post.id))
