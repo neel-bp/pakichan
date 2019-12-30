9b985f8384511d29be4bb7ccbc1f80d240588c62
neelu0 <neelu0@protonmail.com>
2019-12-30 13:56:42 +0500

    better quality thumbnails

--- flaskblog/utilfuncs.py
+++ flaskblog/utilfuncs.py
@@ -13,12 +13,16 @@ def thread_save_picture(form_picture):
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/pics', picture_fn)
     thumb_path = os.path.join(app.root_path, 'static/thumbs', random_hex)
-    size = 250, 250
     i = Image.open(form_picture)
+    width, height = i.size
+    if width <= 250:
+        size = width,width
+    else:
+        size = 250,250
     i.save(picture_path)
     t = Image.open(form_picture).convert('RGB')
     t.thumbnail(size, Image.ANTIALIAS)
-    t.save(thumb_path,'jpeg')
+    t.save(thumb_path,'jpeg',quality=95)
     return picture_fn
 
 def post_save_picture(form_picture):
@@ -27,12 +31,16 @@ def post_save_picture(form_picture):
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/pics', picture_fn)
     thumb_path = os.path.join(app.root_path, 'static/thumbs', random_hex)
-    size = 125, 125
     i = Image.open(form_picture)
+    width,height = i.size
+    if width <= 125:
+        size = width,width
+    else:
+        size = 125,125
     i.save(picture_path)
     t = Image.open(form_picture).convert('RGB')
     t.thumbnail(size, Image.ANTIALIAS)
-    t.save(thumb_path,'jpeg')
+    t.save(thumb_path,'jpeg',quality=95)
     return picture_fn
 
 def utc_to_local(utc_d):
