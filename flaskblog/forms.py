f40e41564063f30c7263f79a0b542df19c3f8760
neelu0 <neelu0@protonmail.com>
2020-02-08 19:14:19 +0500

    added char limit for content field and also title and name field

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
 
 class PostForm(FlaskForm):
-    name = StringField('Name')
-    title = StringField('Title')
-    content = TextAreaField('Content', validators=[DataRequired()])
+    name = StringField('Name', validators=[Length(max=100)])
+    title = StringField('Title', validators=[Length(max=100)])
+    content = TextAreaField('Content', validators=[DataRequired(), Length(max=2000)])
     submit = SubmitField('Post')
     image = FileField('Image',validators=[
         FileRequired(),
@@ -16,8 +16,8 @@ class PostForm(FlaskForm):
     ])
 
 class SubPostForm(FlaskForm):
-    name = StringField('Name')
-    content = TextAreaField('Content', validators=[DataRequired()])
+    name = StringField('Name', validators=[Length(max=100)])
+    content = TextAreaField('Content', validators=[DataRequired(), Length(max=2000)])
     submit = SubmitField('Post')
     image = FileField('Image',validators=[
         FileAllowed(['jpg','png'])
